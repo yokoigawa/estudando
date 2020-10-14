0be380e8395454164b92c276c3acc6c639d4927d
kenji Yokoigawa Jr <72766862+yokoigawa@users.noreply.github.com>
2020-10-14 08:02:58 -0300

    atualização

---
+++ js.aula/aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+console.log(`Viver em ${país}`)
+if (país != 'Brasil') {
+    console.log(`Você é Estrangeiro`)
+}
+else{
+    console.log(`Você é Brasileiro`)
+}
---
+++ js.aula/aula12ex/script.js
@@ -0,0 +1,22 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 8
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        // BOM DIA!
+        img.src = 'manha.png'
+        document.body.style.background = '#DDB186'
+    }else if (hora >= 12 && hora <= 18){
+        //BOA TARDE!
+        img.src = 'tarde.png'
+        document.body.style.background = '#73696B'
+    }else{
+        //BOA NOITE!
+        img.src = 'noite.png'
+        document.body.style.background = '#281D0F'
+    }
+
+}
